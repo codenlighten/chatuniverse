d2e9170bd63133ec181f76518c220c68d9d0204b
greg <codenlighten1@gmail.com>
2023-12-30 07:51:42 -0500

    notifications

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,24 +1,27 @@
 // service-worker.js
-self.addEventListener("push", function (event) {
-  const data = event.data.json();
-  const options = {
-    body: data.body,
-    icon: "satochat.png",
-    badge: "satochat.png",
-    image: "satochat.png",
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: 1,
-    },
-    actions: [
-      {
-        action: "explore",
-        title: "New Message",
-        icon: "satochat.png",
+self.addEventListener("message", function (event) {
+  if (event.data && event.data.type === "newMessage") {
+    const messageData = event.data.data;
+    const title = "New Chat Message";
+    const options = {
+      body: messageData.decrypted,
+      icon: "satochat.png",
+      badge: "satochat.png",
+      image: "satochat.png",
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: 1,
       },
-      { action: "close", title: "Close", icon: "satochat.png" },
-    ],
-  };
-  event.waitUntil(self.registration.showNotification(data.title, options));
+      actions: [
+        {
+          action: "explore",
+          title: "New Message",
+          icon: "satochat.png",
+        },
+        { action: "close", title: "Close", icon: "satochat.png" },
+      ],
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+  }
 });
